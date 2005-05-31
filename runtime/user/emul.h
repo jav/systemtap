9c5d496e200d5476b126bc5ff9806365a77a2c7e
hunt <hunt>
2005-05-31 19:58:29 +0000

    *** empty log message ***

--- runtime/user/emul.h
+++ runtime/user/emul.h
@@ -14,3 +14,19 @@ int vscnprintf(char *buf, size_t size, const char *fmt, va_list args)
 #include <stdarg.h>
 unsigned long strtoul(const char *nptr, char **endptr, int base);
 #define simple_strtoul strtoul
+
+#define KALLSYMS_LOOKUP kallsyms_lookup
+
+const char *kallsyms_lookup (unsigned long addr,
+			     unsigned long *symbolsize,
+			     unsigned long *offset,
+			     char **modname, 
+			     char *namebuf)
+{
+  static char buf[32];
+  sprintf (namebuf, "foobar");
+  sprintf (buf, "foobar_mod");
+  *offset = 1;
+  modname = (char **)&buf;
+  return namebuf;
+}
