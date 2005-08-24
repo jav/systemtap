d072ec9d384f4f6072317e530b42a834f0982921
hunt <hunt>
2005-08-24 16:29:46 +0000

    2005-08-24  Martin Hunt  <hunt@redhat.com>
    	* io.c (_stp_vlog): Send warnings and errors to STP_OOB_DATA.

--- runtime/io.c
+++ runtime/io.c
@@ -57,12 +57,12 @@ static void _stp_vlog (enum code type, char *func, int line, const char *fmt, va
 		
 #ifdef STP_RELAYFS
 		if (type != DBUG)
-			_stp_write(STP_REALTIME_DATA, buf, start + num + 1);
+			_stp_write(STP_OOB_DATA, buf, start + num + 1);
 		_stp_string_cat_cstr(_stp_stdout,buf);
 		_stp_print_flush();
 #else
 		if (type != DBUG)
-			_stp_write(STP_REALTIME_DATA, buf, start + num + 1);
+			_stp_write(STP_OOB_DATA, buf, start + num + 1);
 		else {
 			_stp_string_cat_cstr(_stp_stdout,buf);
 			_stp_print_flush();
