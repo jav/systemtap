ae653153d30946a7706369486dc646a9a3839a7c
hunt <hunt>
2006-03-30 21:01:20 +0000

    2006-03-30  Martin Hunt  <hunt@redhat.com>
    	* emul.h (kmalloc_node): New.

--- runtime/user/emul.h
+++ runtime/user/emul.h
@@ -1,4 +1,6 @@
 #define kmalloc(size,flags) malloc(size)
+#define kmalloc(size,flags) malloc(size)
+#define kmalloc_node(size,flags,cpu) malloc(size)
 
 #define kfree(ptr) free(ptr)
 
@@ -87,4 +89,33 @@ void __lockfunc _spin_lock(spinlock_t *lock)
 
 void __lockfunc _spin_unlock(spinlock_t *lock)
 {
+}
+
+size_t strlcat(char *dest, const char *src, size_t count)
+{
+	size_t dsize = strlen(dest);
+	size_t len = strlen(src);
+	size_t res = dsize + len;
+
+	/* This would be a bug */
+	BUG_ON(dsize >= count);
+
+	dest += dsize;
+	count -= dsize;
+	if (len >= count)
+		len = count-1;
+	memcpy(dest, src, len);
+	dest[len] = 0;
+	return res;
+}
+size_t strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t ret = strlen(src);
+
+	if (size) {
+		size_t len = (ret >= size) ? size - 1 : ret;
+		memcpy(dest, src, len);
+		dest[len] = '\0';
+	}
+	return ret;
 }
