de2ca07e3fd2290c5a119f0c891d6474215ec95c
trz <trz>
2006-04-12 22:58:17 +0000

    Fix PR 2538

--- buildrun.cxx
+++ buildrun.cxx
@@ -72,7 +72,7 @@ compile_pass (systemtap_session& s)
 
   // Assumes linux 2.6 kbuild
   o << "CFLAGS += -Wno-unused -Werror" << endl;
-  o << "CFLAGS += -I \"" << s.runtime_path << "\"" << endl;
+  o << "CFLAGS += -I\"" << s.runtime_path << "\"" << endl;
   o << "obj-m := " << s.module_name << ".o" << endl;
 
   o.close ();
