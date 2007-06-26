9858f23c4fe3856055e0ded2019d17e4a873091a
wcohen <wcohen>
2007-06-26 20:55:27 +0000

    Remove stray "\n" from probe_point::print.

--- staptree.cxx
+++ staptree.cxx
@@ -868,7 +868,7 @@ void probe_point::print (ostream& o) const
     }
   if (optional)
     o << "?";
-  o << "{" << tok->location << "}\n";
+  o << "{" << tok->location << "}";
 }
 
 string probe_point::str ()
