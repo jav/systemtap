d1cd4267e903d8bb0190e96e572ed42fcd0b25d1
hunt <hunt>
2007-07-03 21:38:40 +0000

    2007-07-03  Martin Hunt  <hunt@redhat.com>
    	* systemtap.context/pid.tcl: Better error reporting.
    	* systemtap.context/args.*: Ditto.
    	* systemtap.context/context.exp: Build in temp dir.

--- testsuite/systemtap.context/args.tcl
+++ testsuite/systemtap.context/args.tcl
@@ -8,6 +8,7 @@ expect {
 	    "yyy_int -1 200 300\r\nyyy_int returns 499\r\n" {
 		pass "integer function arguments"
 	    }
+	    timeout {fail "integer function arguments"}
 	}
 	exec echo 2 > /proc/stap_test_cmd
 	expect {
@@ -15,6 +16,7 @@ expect {
 	    "yyy_uint 4294967295 200 300\r\nyyy_uint returns 499\r\n" {
 		pass "unsigned function arguments"
 	    }
+	    timeout {fail "unsigned function arguments"}
 	}
 	exec echo 3 > /proc/stap_test_cmd
 	expect {
@@ -22,6 +24,7 @@ expect {
 	    "yyy_long -1 200 300\r\nyyy_long returns 499\r\n" {
 		pass "long function arguments"
 	    }
+	    timeout {fail "long function arguments"}
 	}
 	exec echo 4 > /proc/stap_test_cmd
 	expect {
@@ -29,13 +32,15 @@ expect {
 	    "yyy_int64 -1 200 300\r\nyyy_int64 returns 499\r\n" {
 		pass "int64 function arguments"
 	    }
+	    timeout {fail "int64 function arguments"}
 	}
 	exec echo 5 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
-	    "yyy_char 97 98 99\r\nyyy_char returns 81\r\n" {
+	    "yyy_char a b c\r\nyyy_char returns Q\r\n" {
 		pass "char function arguments"
 	    }
+	    timeout {fail "char function arguments"}
 	}
 	exec echo 6 > /proc/stap_test_cmd
 	expect {
@@ -43,9 +48,10 @@ expect {
 	    "yyy_str Hello-System-Tap\r\nyyy_str returns XYZZY\r\n" {
 		pass "string function arguments"
 	    }
+	    timeout {fail "string function arguments"}
 	}
     }
-    eof {}
+    eof {fail "function arguments: unexpected timeout"}
 }
 send "\003"
 close
--- testsuite/systemtap.context/pid.tcl
+++ testsuite/systemtap.context/pid.tcl
@@ -1,54 +1,31 @@
+set tests [list execname pexecname pid ppid tid uid euid gid egid]
 spawn stap pid.stp
 expect {
     -timeout 240
     "READY" {
 	set pid [exec echo 1 > /proc/stap_test_cmd &]
-	set uid [exec id -ru] 
-	set gid [exec id -rg] 
-	set euid [exec id -u] 
-	set egid [exec id -g] 
-	expect {
-            -timeout 5
-            "execname: echo\r\n" {
-                pass "execname"
-		exp_continue
-            }
-	    "pexecname: expect\r\n" {
-                pass "pexecname"
-		exp_continue
+	set ppid [lindex [split [exec grep PPid /proc/$pid/status]] 1]
+	set uid [exec id -ru]
+	set gid [exec id -rg]
+	set euid [exec id -u]
+	set egid [exec id -g]
+	set results [list "execname: echo\r\n" "pexecname: expect\r\n" "pid: $pid\r\n" "ppid: $ppid\r\n" "tid: $pid\r\n" "uid: $uid\r\n" "euid: $euid\r\n" "gid: $gid\r\n" "egid: $egid\r\n"]
+	
+	set i 0
+	foreach t $tests {
+	    expect {
+		-timeout 5
+		[lindex $results $i] {
+		    pass $t
+		}
+		timeout {fail "$t - timeout"}
+		eof {fail "$t - unexpected EOF"}
 	    }
-	    "pid: $pid\r\n" {
-		pass "pid"
-		exp_continue
-	    }
-	    -re {ppid: [^\r\n]+\r\n} {
-		pass "ppid"
-		exp_continue
-	    }
-	    "tid: $pid\r\n" {
-		pass "tid"
-		exp_continue
-	    }
-	    "uid: $uid\r\n" {
-		pass "uid"
-		exp_continue
-	    }
-	    "euid: $euid\r\n" {
-		pass "euid"
-		exp_continue
-	    }
-	    "gid: $gid\r\n" {
-		pass "gid"
-		exp_continue
-	    }
-	    "egid: $egid\r\n" {
-		pass "egid"
-		exp_continue
-	    }
-	    eof {}
-        }
+	    incr i
+	}
     }
-    eof {}
+    timeout {fail "all pid tests - timeout"}
+    eof {fail "all pid tests - unexpected EOF"}
 }
 catch {close}
 wait
