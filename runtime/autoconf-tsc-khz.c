333fe7b868dcd622b4c3b50afb4c6e833a260df0
hiramatu <hiramatu>
2007-10-17 22:10:23 +0000

    2007-10-17  Masami Hiramatsu <mhiramat@redhat.com>
            * autoconf-tsc-khz.c: Not to be compiled if the kernel version is
            younger than 2.6.23 on i386.

--- runtime/autoconf-tsc-khz.c
+++ runtime/autoconf-tsc-khz.c
@@ -1,3 +1,7 @@
+#include <linux/version.h>
 #include <asm/tsc.h>
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23)) && defined(__i386__)
+#error "tsc_khz is not exported"
+#endif
 unsigned int *ptsc = &tsc_khz;
