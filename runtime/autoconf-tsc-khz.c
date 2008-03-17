3e164074c421d45f63f43e1fde3be78a43428e4c
eteo <eteo>
2008-03-17 14:46:49 +0000

    2008-03-17  Eugene Teo  <eteo@redhat.com>
    	PR 5947
    	* autoconf-tsc-khz.c: Remove "&& defined(__i386__)" to make the test
    	more pessimistic.

--- runtime/autoconf-tsc-khz.c
+++ runtime/autoconf-tsc-khz.c
@@ -1,7 +1,7 @@
 #include <linux/version.h>
 #include <asm/tsc.h>
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23)) && defined(__i386__)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23))
 #error "tsc_khz is not exported"
 #endif
 unsigned int *ptsc = &tsc_khz;
