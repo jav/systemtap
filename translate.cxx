6d05e34f799ac14ccd7f0adab39986b8b6d89939
David Smith <dsmith@redhat.com>
2008-04-10 15:00:08 -0500

    2008-04-10  David Smith  <dsmith@redhat.com>
    	* elaborate.h (struct derived_probe_group): Added
    	emit_module_header virtual function.
    	* translate.cxx (c_unparser::emit_common_header): Calls each probe
    	group's emit_module_header function.
    	(translate_pass): Moved inclusion of linux/marker.h to
    	mark_derived_probe_group::emit_module_header().
    	* tapsets.cxx (struct be_derived_probe_group): Added empty
    	emit_module_header function.
    	(struct dwarf_derived_probe_group): Ditto.
    	(struct timer_derived_probe_group): Ditto.
    	(struct profile_derived_probe_group): Ditto.
    	(struct procfs_derived_probe_group): Ditto.
    	(struct hrtimer_derived_probe_group): Ditto.
    	(struct perfmon_derived_probe_group): Ditto.
    	(uprobe_derived_probe_group::emit_module_header): Moved uprobe
    	kernel check from emit_module_decls() to here.
    	(uprobe_derived_probe_group::emit_module_decls): Moved uprobe
    	kernel check to emit_module_header().
    	(mark_derived_probe_group::emit_module_header): Moved marker
    	kernel check from emit_module_decls and translate_pass() to here.
    	(uprobe_derived_probe_group::emit_module_decls): Moved marker
    	kernel check to emit_module_header().

--- translate.cxx
+++ translate.cxx
@@ -849,6 +849,11 @@ translator_output::line ()
 void
 c_unparser::emit_common_header ()
 {
+  vector<derived_probe_group*> g = all_session_groups (*session);
+  for (unsigned i=0; i<g.size(); i++)
+    g[i]->emit_module_header (*session);
+  
+  o->newline();
   o->newline() << "typedef char string_t[MAXSTRINGLEN];";
   o->newline();
   o->newline() << "#define STAP_SESSION_STARTING 0";
@@ -4524,10 +4529,6 @@ translate_pass (systemtap_session& s)
       // XXX: old 2.6 kernel hack
       s.op->newline() << "#ifndef read_trylock";
       s.op->newline() << "#define read_trylock(x) ({ read_lock(x); 1; })";
-      s.op->newline() << "#endif";
-
-      s.op->newline() << "#if defined(CONFIG_MARKERS)";
-      s.op->newline() << "#include <linux/marker.h>";
       s.op->newline() << "#endif";
 
       s.up->emit_common_header (); // context etc.
