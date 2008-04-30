3b5ab982d758481ccd5003348391bea0b775d34b
Masami Hiramatsu <mhiramat@redhat.com>
2008-04-30 11:48:25 -0400

    PR5645: Fix sub-buffer size calculation and debug messages.

--- runtime/staprun/staprun.c
+++ runtime/staprun/staprun.c
@@ -230,7 +230,7 @@ int main(int argc, char **argv)
 	parse_args(argc, argv);
 
 	if (buffer_size)
-		dbug(2, "Using a buffer of %u bytes.\n", buffer_size);
+		dbug(2, "Using a buffer of %u MB.\n", buffer_size);
 
 	if (optind < argc) {
 		parse_modpath(argv[optind++]);
