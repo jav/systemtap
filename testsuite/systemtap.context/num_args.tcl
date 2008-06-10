d63de374bbda0c742465e8b09248fb0aace67df8
Jim Keniston <jkenisto@us.ibm.com>
2008-06-10 12:11:28 -0700

    Enable and update numeric-arg tests.

--- testsuite/systemtap.context/num_args.tcl
+++ testsuite/systemtap.context/num_args.tcl
@@ -1,4 +1,7 @@
-spawn stap $srcdir/$subdir/num_args.stp
+set arglists {{} {--kelf --ignore-dwarf}}
+foreach arglist $arglists {
+set tag [concat numeric $arglist]
+eval spawn stap $arglist $srcdir/$subdir/num_args.stp
 expect {
     -timeout 240
     "READY" {
@@ -6,53 +9,54 @@ expect {
 	expect {
 	    -timeout 5
 	    "yyy_int -1 200 300\r\nyyy_int returns 499\r\n" {
-		pass "integer function arguments (numeric)"
+		pass "integer function arguments -- $tag"
 	    }
-	    timeout {fail "integer function arguments (numeric)"}
+	    timeout {fail "integer function arguments -- $tag"}
 	}
 	exec echo 2 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
 	    "yyy_uint 4294967295 200 300\r\nyyy_uint returns 499\r\n" {
-		pass "unsigned function arguments (numeric)"
+		pass "unsigned function arguments -- $tag"
 	    }
-	    timeout {fail "unsigned function arguments (numeric)"}
+	    timeout {fail "unsigned function arguments -- $tag"}
 	}
 	exec echo 3 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
 	    "yyy_long -1 200 300\r\nyyy_long returns 499\r\n" {
-		pass "long function arguments (numeric)"
+		pass "long function arguments -- $tag"
 	    }
-	    timeout {fail "long function arguments (numeric)"}
+	    timeout {fail "long function arguments -- $tag"}
 	}
 	exec echo 4 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
 	    "yyy_int64 -1 200 300\r\nyyy_int64 returns 499\r\n" {
-		pass "int64 function arguments (numeric)"
+		pass "int64 function arguments -- $tag"
 	    }
-	    timeout {fail "int64 function arguments (numeric)"}
+	    timeout {fail "int64 function arguments -- $tag"}
 	}
 	exec echo 5 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
 	    "yyy_char a b c\r\nyyy_char returns Q\r\n" {
-		pass "char function arguments (numeric)"
+		pass "char function arguments -- $tag"
 	    }
-	    timeout {fail "char function arguments (numeric)"}
+	    timeout {fail "char function arguments -- $tag"}
 	}
 	exec echo 6 > /proc/stap_test_cmd
 	expect {
 	    -timeout 5
 	    "yyy_str Hello-System-Tap\r\nyyy_str returns XYZZY\r\n" {
-		pass "string function arguments (numeric)"
+		pass "string function arguments -- $tag"
 	    }
-	    timeout {fail "string function arguments (numeric)"}
+	    timeout {fail "string function arguments -- $tag"}
 	}
     }
-    eof {fail "function arguments (numeric): unexpected timeout"}
+    eof {fail "function arguments -- $tag: unexpected timeout"}
 }
 exec kill -INT -[exp_pid]
 close
 wait
+}
