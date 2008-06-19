1773fd0385b253ae255aa686e4f8de4764b016f4
Josh Stone <joshua.i.stone@intel.com>
2008-06-18 19:14:15 -0700

    Add Intel to copyright header on elaborate.cxx

--- elaborate.cxx
+++ elaborate.cxx
@@ -1,5 +1,6 @@
 // elaboration functions
 // Copyright (C) 2005-2008 Red Hat Inc.
+// Copyright (C) 2008 Intel Corporation
 //
 // This file is part of systemtap, and is free software.  You can
 // redistribute it and/or modify it under the terms of the GNU General
