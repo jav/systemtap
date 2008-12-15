6e44f060ffb5fff8d3987024d8facfb7997a3b25
Mark Wielaard <mjw@redhat.com>
2008-12-15 18:20:04 +0100

    Compile and use helper usymbols_lib.c library for usymbols.exp test.

--- testsuite/systemtap.context/usymbols_lib.c
+++ testsuite/systemtap.context/usymbols_lib.c
@@ -1,4 +1,4 @@
-/* usymbol test case
+/* usymbol test case - library helper
  * Copyright (C) 2008, Red Hat Inc.
  *                                                          
  * This file is part of systemtap, and is free software.  You can
@@ -8,32 +8,22 @@
  *
  * Uses signal to tranfer user space addresses into the kernel where a
  * probe on sigaction will extract them and produce the symbols.  To
- * poke into the executable we get the sa_handler from the main executable,
- * and then the library through calling signal.
- *
- * FIXME. We call into the library to get the right symbol. If we
- * register the handler from the main executable. We need to handle
- * @plt symbols (setting a handler in the main executable that is in a
- * shared library will have the @plt address, not the address inside
- * the shared library).
+ * poke into the executable we get the sa_handler set through signal
+ * from this library.
  */
 
 #include <signal.h>
 typedef void (*sighandler_t)(int);
 
-// function from our library
-int lib_main (void);
-
 void
-main_handler (int signum)
+lib_handler (int signum)
 {
   /* dummy handler, just used for the address... */
 }
 
-int
-main (int argc, char *argv[], char *envp[])
+void
+lib_main ()
 {
   // Use SIGFPE since we never expect that to be triggered.
-  signal(SIGFPE, main_handler);
-  lib_main();
+  signal(SIGFPE, lib_handler);
 }
