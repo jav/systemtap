ffaf6b84d9a3820d538e2d20df0e19e7fd0607d8
ddomingo <ddomingo@redhat.com>
2009-01-28 11:24:31 +1000

    cleaner versions

--- doc/SystemTap_Beginners_Guide/syncandbuild.sh
+++ doc/SystemTap_Beginners_Guide/syncandbuild.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 # Instead of running the original makefile to build the document, run this script instead
-echo -n "Please specify your build target (e.g. html-en-US, pdf-en-US, etc)	"
+echo -n "Please specify your build target (e.g. html, pdf, or html-single)	"
 read TARG
-echo -n "Please specify the product you are building for: enter 1 for Fedora, 2 for RHEL	"
+echo -n "Please specify the product you are building for (enter 1 for Fedora, 2 for RHEL)	"
 read PROD
-echo -n "Thank you. Now specify any build parameters you'd like to use (skip this step for none).	"
+echo -n "Please specify any build parameters you'd like to use (skip this step for none).	"
 read PARM
 
-cp -a ../../testsuite  en-US/extras/.
-
 if [ $PROD = 1 ]; 
-	then 	
+then 	
 	sed -i -e 's/<productname>Red Hat Enterprise Linux/<productname>Fedora/g' en-US/Book_Info.xml;
 	sed -i -e 's/<productnumber>5/<productnumber>10/g' en-US/Book_Info.xml;
 	sed -i -e 's/BRAND = RedHat/BRAND = fedora/g' Makefile;
-	make $PARM $TARG
+	make $PARM $TARG-en-US post
 
-else make $PARM $TARG
+else 
+sed -i -e 's/BRAND = fedora/BRAND = RedHat/g' Makefile;	
+make post $PARM $TARG-en-US
 fi
 
-echo "Setting default brand back to RHEL (if needed)..." 
-	sed -i -e 's/<productname>Fedora/<productname>Red Hat Enterprise Linux/g' en-US/Book_Info.xml;
-	sed -i -e 's/<productnumber>10/<productnumber>5/g' en-US/Book_Info.xml;
-	sed -i -e 's/BRAND = fedora/BRAND = RedHat/g' Makefile;	
 echo "done."
 echo "Cleaning sync'd files..."
 make post
