4d0a258fa72d08fc72faa48d443d34269544b9da
ddomingo <ddomingo@redhat.com>
2009-02-06 14:36:00 +1000

    Tapset Ref Guide: corrected authorgroup cleanup perl script, added support for forced Synopsis

--- doc/Tapset_Reference_Guide/publicanize.sh
+++ doc/Tapset_Reference_Guide/publicanize.sh
@@ -6,11 +6,15 @@ cp ../SystemTap_Tapset_Reference/tapsets.xml en-US/Tapset_Reference_Guide.xml ;
 #remove all excess whitespace
 sed -i -e 's/^\s*//g' en-US/Tapset_Reference_Guide.xml ;
 
+#re-convert programlisting tags
+sed -i  -e 's/&lt;programlisting&gt;/<programlisting>/g' en-US/Tapset_Reference_Guide.xml;
+sed -i  -e 's/&lt;\/programlisting&gt;/<\/programlisting>/g' en-US/Tapset_Reference_Guide.xml;
+
 #replace header
 
 cat en-US/Tapset_Reference_Guide.xml | 
 perl -p -e 'undef $/;s|<bookinfo>\n<title>SystemTap Tapset Reference Manual</title>|<xi:include href="Book_Info.xml" xmlns:xi="http://www.w3.org/2001/XInclude" />\n<xi:include href="Preface.xml" xmlns:xi="http://www.w3.org/2001/XInclude" />|msg' | 
-perl -p -e 'undef $/;s|<authorgroup>\n<author>\n<firstname>William</firstname>\n<surname>Cohen</surname>\n<affiliation>\n<address>\n<email>wcohen\@redhat.com</email>\n</address>\n</affiliation>\n</author>\n</authorgroup>||msg' |
+perl -p -e 'undef $/;s|<authorgroup>\n<author>\n<firstname>William</firstname>\n<surname>Cohen</surname>\n<contrib></contrib>\n<affiliation>\n<address>\n<email>wcohen\@redhat.com</email>\n</address>\n</affiliation>\n</author>\n</authorgroup>||msg' |
 perl -p -e 'undef $/;s|<copyright>\n<year>2008, 2009</year>\n<holder>Red Hat, Inc.</holder>\n</copyright>||msg' |
 perl -p -e 'undef $/;s|<legalnotice>\n<para>\nThis documentation is free software\; you can redistribute\nit and/or modify it under the terms of the GNU General Public\nLicense version 2 as published by the Free Software Foundation.\n</para>||msg' | 
 perl -p -e 'undef $/;s|<para>\nThis program is distributed in the hope that it will be\nuseful, but WITHOUT ANY WARRANTY; without even the implied\nwarranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n</para>||msg' | 
@@ -20,7 +24,9 @@ perl -p -e 'undef $/;s|<toc></toc>||msg' |
 perl -p -e 'undef $/;s|\n\n\n\n\n\n\n\n\n\n\n\n\n\n||msg' | 
 perl -p -e 'undef $/;s|<programlisting>\n|<programlisting>\n<emphasis>(sfunction) <\/emphasis>|msg' | 
 perl -p -e 'undef $/;s|<para>\n</para>||msg' | 
-perl -p -e 'undef $/;s|<para>\n\n</para>||msg' > clean.xml
+perl -p -e 'undef $/;s|<para>\n\n</para>||msg' | 
+perl -p -e 'undef $/;s|<para>\n<programlisting>|<programlisting>|msg' |
+perl -p -e 'undef $/;s|</programlisting>\n</para>|</programlisting>|msg' > clean.xml
 
 cp clean.xml en-US/Tapset_Reference_Guide.xml
 rm clean.xml
