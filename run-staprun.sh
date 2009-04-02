65ffc3f32328473cb74aa5c7eca7e46bb82bd7fb
Josh Stone <jistone@redhat.com>
2009-04-01 19:41:55 -0700

    Fix the magic run-stap wrappers even more

--- run-staprun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-exec sudo env builddir="${builddir}" \
-     	      SYSTEMTAP_STAPIO="${builddir}/run-stapio" \
-	      SYSTEMTAP_STAPRUN="$0" \
-     	  "${builddir}/staprun" ${1+"$@"}
