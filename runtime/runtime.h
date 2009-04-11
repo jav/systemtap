a6f6008cf0034b8580f67851f742dd0ada0d683c
Mark Wielaard <mjw@redhat.com>
2009-04-11 13:08:06 +0200

    Prefer dwarf unwinder on i386 and x86_64.
    * runtime/runtime.h: Move up and enable check for STP_USE_DWARF_UNWINDER.

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -66,18 +66,16 @@ static struct
 #define MAXTRACE 20
 #endif
 
+/* dwarf unwinder only tested so far on i386 and x86_64. */
+#if (defined(__i386__) || defined(__x86_64__))
+#define STP_USE_DWARF_UNWINDER
+#endif
+
 #ifdef CONFIG_FRAME_POINTER
 /* Just because frame pointers are available does not mean we can trust them. */
 #ifndef STP_USE_DWARF_UNWINDER
 #define STP_USE_FRAME_POINTER
 #endif
-#endif
-
-/* dwarf unwinder only tested so far on i386 and x86_64,
-   but globally disabled for now */
-#if 0
-// !defined(STP_USE_FRAME_BUFFER) && (defined(__i386__) || defined(__x86_64__))
-#define STP_USE_DWARF_UNWINDER
 #endif
 
 #include "alloc.c"
