8823ee6a3fcf47adbb7314c6fd560e6c952c4705
Mark Wielaard <mjw@redhat.com>
2009-07-29 23:26:26 +0200

    Remove unused result_string from ustack.tcl.
    * testsuite/systemtap.exelib/ustack.tcl: Remove result_string.

--- testsuite/systemtap.exelib/ustack.tcl
+++ testsuite/systemtap.exelib/ustack.tcl
@@ -1,12 +1,3 @@
-set ::result_string {exe: main=main
-exe: main_func=main_func
-exe: main_func=main_func
-exe: main_func=main_func
-lib: lib_main=lib_main
-lib: lib_func=lib_func
-lib: lib_func=lib_func
-    lib: lib_func=lib_func}
-
 # Only run on make installcheck
 if {! [installtest_p]} { untested "ustack-$testname"; return }
 if {! [utrace_p]} { untested "ustack-$testname"; return }
