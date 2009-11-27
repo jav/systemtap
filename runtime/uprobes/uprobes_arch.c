01f8d9fcc10100b16391cf797dc0647e7112c5a9
Anton Vorontsov <avorontsov@ru.mvista.com>
2009-11-28 01:33:36 +0300

    Rename uprobes_ppc64.c to uprobes_ppc.c, use it for ppc32
    The code *looks* generic enough, so I think it can be used for ppc32
    without modifications.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>

--- runtime/uprobes/uprobes_arch.c
+++ runtime/uprobes/uprobes_arch.c
@@ -2,8 +2,8 @@
 #include "uprobes_x86_64.c"
 #elif defined (__i386__)
 #include "uprobes_i386.c"
-#elif defined (__powerpc64__)
-#include "uprobes_ppc64.c"
+#elif defined (__powerpc__)
+#include "uprobes_ppc.c"
 #elif defined (__s390__) || defined (__s390x__)
 #include "uprobes_s390.c"
 #else
--- runtime/uprobes2/uprobes_arch.c
+++ runtime/uprobes2/uprobes_arch.c
@@ -1,7 +1,7 @@
 #if defined (__x86_64__) || defined(__i386)
 #include "uprobes_x86.c"
-#elif defined (__powerpc64__)
-#include "../uprobes/uprobes_ppc64.c"
+#elif defined (__powerpc__)
+#include "../uprobes/uprobes_ppc.c"
 #elif defined (__s390__) || defined (__s390x__)
 #include "../uprobes/uprobes_s390.c"
 #elif defined (__ia64__)
