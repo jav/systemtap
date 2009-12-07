f669d095ba7fe5a623b31abc05b4f6664059803b
Tim Moore <timoore@redhat.com>
2009-12-07 19:19:45 +0100

    add copyright and license to grapher files

--- grapher/GraphData.hxx
+++ grapher/GraphData.hxx
@@ -1,3 +1,11 @@
+// systemtap grapher
+// Copyright (C) 2009 Red Hat Inc.
+//
+// This file is part of systemtap, and is free software.  You can
+// redistribute it and/or modify it under the terms of the GNU General
+// Public License (GPL); either version 2, or (at your option) any
+// later version.
+
 #ifndef SYSTEMTAP_GRAPHDATA_HXX
 #define SYSTEMTAP_GRAPHDATA_HXX 1
 
--- grapher/StapParser.cxx
+++ grapher/StapParser.cxx
@@ -1,3 +1,11 @@
+// systemtap grapher
+// Copyright (C) 2009 Red Hat Inc.
+//
+// This file is part of systemtap, and is free software.  You can
+// redistribute it and/or modify it under the terms of the GNU General
+// Public License (GPL); either version 2, or (at your option) any
+// later version.
+
 #include "StapParser.hxx"
 
 #include <unistd.h>
