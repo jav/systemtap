32902edf1f17bb62144f21ed38ae085bd2755e9f
Tim Moore <timoore@redhat.com>
2009-12-21 10:46:57 +0100

    change namespace indentation to 0
    A silly change, but this works better with other tools, like git
    diff. I should have started the C++ files this way.

--- grapher/Graph.hxx
+++ grapher/Graph.hxx
@@ -16,46 +16,46 @@
 
 namespace systemtap
 {
-  class Graph : public CairoWidget
-  {
-  public:
-    friend class GraphWidget;
-    Graph(double x = 0.0, double y = 0.0);
-    virtual void draw(Cairo::RefPtr<Cairo::Context> cr);
-    virtual bool containsPoint(double x, double y);
-    double getLineWidth() { return _lineWidth; }
-    void setLineWidth(double lineWidth) { _lineWidth = lineWidth; }
-    bool getAutoScaling() const { return _autoScaling; }
-    void setAutoScaling(bool val) { _autoScaling = val; }
-    void addGraphData(std::tr1::shared_ptr<GraphDataBase> data);
-    void getExtents(int64_t& left, int64_t& right, double& top, double& bottom)
-        const;
-    void setExtents(int64_t left, int64_t right, double top, double bottom);
-    // extents of the whole graph area
-    double _width;
-    double _height;
-    // Position, extents of the graph
-    double _graphX;
-    double _graphY;
-    double _graphWidth;
-    double _graphHeight;
-    double _lineWidth;
-    bool _autoScaling;
-    bool _autoScrolling;
-    double _zoomFactor;
-    double _xOffset;
-    double _yOffset;
-    std::tr1::shared_ptr<CairoPlayButton> _playButton;
-    int64_t _timeBase;
-    GraphDataList& getDatasets() { return _datasets; }
-    int64_t getTimeAtPoint(double x);
-    void window2GraphCoords(double x, double y, double& xgraph, double& ygraph);
-  protected:
-    GraphDataList _datasets;
-    int64_t _left;
-    int64_t _right;
-    double _top;
-    double _bottom;
-  };
+class Graph : public CairoWidget
+{
+public:
+  friend class GraphWidget;
+  Graph(double x = 0.0, double y = 0.0);
+  virtual void draw(Cairo::RefPtr<Cairo::Context> cr);
+  virtual bool containsPoint(double x, double y);
+  double getLineWidth() { return _lineWidth; }
+  void setLineWidth(double lineWidth) { _lineWidth = lineWidth; }
+  bool getAutoScaling() const { return _autoScaling; }
+  void setAutoScaling(bool val) { _autoScaling = val; }
+  void addGraphData(std::tr1::shared_ptr<GraphDataBase> data);
+  void getExtents(int64_t& left, int64_t& right, double& top, double& bottom)
+    const;
+  void setExtents(int64_t left, int64_t right, double top, double bottom);
+  // extents of the whole graph area
+  double _width;
+  double _height;
+  // Position, extents of the graph
+  double _graphX;
+  double _graphY;
+  double _graphWidth;
+  double _graphHeight;
+  double _lineWidth;
+  bool _autoScaling;
+  bool _autoScrolling;
+  double _zoomFactor;
+  double _xOffset;
+  double _yOffset;
+  std::tr1::shared_ptr<CairoPlayButton> _playButton;
+  int64_t _timeBase;
+  GraphDataList& getDatasets() { return _datasets; }
+  int64_t getTimeAtPoint(double x);
+  void window2GraphCoords(double x, double y, double& xgraph, double& ygraph);
+protected:
+  GraphDataList _datasets;
+  int64_t _left;
+  int64_t _right;
+  double _top;
+  double _bottom;
+};
 }
 #endif
--- grapher/GraphStyle.hxx
+++ grapher/GraphStyle.hxx
@@ -14,53 +14,53 @@
 
 namespace systemtap
 {
-  class GraphDataBase;
-  class Graph;
+class GraphDataBase;
+class Graph;
 
-  class GraphStyle
+class GraphStyle
+{
+public:
+  virtual void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
+                    Graph* graph, Cairo::RefPtr<Cairo::Context> cr) = 0;
+  virtual ssize_t dataIndexAtPoint(double x, double y,
+                                   std::tr1::shared_ptr<GraphDataBase>
+                                   graphData,
+                                   std::tr1::shared_ptr<Graph> graph)
   {
-  public:
-    virtual void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
-                      Graph* graph, Cairo::RefPtr<Cairo::Context> cr) = 0;
-    virtual ssize_t dataIndexAtPoint(double x, double y,
-                                     std::tr1::shared_ptr<GraphDataBase>
-                                     graphData,
-                                     std::tr1::shared_ptr<Graph> graph)
-    {
-      return -1;
-    }
-  };
+    return -1;
+  }
+};
 
-  class GraphStyleBar : public GraphStyle
-  {
-  public:
-    void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
-              Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
+class GraphStyleBar : public GraphStyle
+{
+public:
+  void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
+            Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
     
-    static GraphStyleBar instance;
-    ssize_t dataIndexAtPoint(double x, double y,
-                             std::tr1::shared_ptr<GraphDataBase> graphData,
-                             std::tr1::shared_ptr<Graph> graph);
-  };
+  static GraphStyleBar instance;
+  ssize_t dataIndexAtPoint(double x, double y,
+                           std::tr1::shared_ptr<GraphDataBase> graphData,
+                           std::tr1::shared_ptr<Graph> graph);
+};
 
-  class GraphStyleDot : public GraphStyle
-  {
-  public:
-    void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
-              Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
-    static GraphStyleDot instance;
-  };
+class GraphStyleDot : public GraphStyle
+{
+public:
+  void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
+            Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
+  static GraphStyleDot instance;
+};
 
-  class GraphStyleEvent : public GraphStyle
-  {
-  public:
-    void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
-              Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
-    virtual ssize_t dataIndexAtPoint(double x, double y,
-                                     std::tr1::shared_ptr<GraphDataBase>
-                                     graphData,
-                                     std::tr1::shared_ptr<Graph> graph);
-    static GraphStyleEvent instance;
-  };  
+class GraphStyleEvent : public GraphStyle
+{
+public:
+  void draw(std::tr1::shared_ptr<GraphDataBase> graphData,
+            Graph* graph, Cairo::RefPtr<Cairo::Context> cr);
+  virtual ssize_t dataIndexAtPoint(double x, double y,
+                                   std::tr1::shared_ptr<GraphDataBase>
+                                   graphData,
+                                   std::tr1::shared_ptr<Graph> graph);
+  static GraphStyleEvent instance;
+};  
 }
 #endif
