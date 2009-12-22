b8b815b7163b3df61a7364e404a282cb17d775db
David Smith <dsmith@redhat.com>
2009-12-21 21:04:36 -0600

    PR11113 fix.  Support new utrace API.
    * tapset-utrace.cxx (utrace_derived_probe_group::emit_module_decls):
      Handles new utrace api.
    * runtime/itrace.c (usr_itrace_report_signal): Ditto.
      (usr_itrace_report_clone): Ditto.
      (usr_itrace_report_death): Ditto.
    * runtime/task_finder.c (__stp_utrace_task_finder_report_clone): Ditto.
      (__stp_utrace_task_finder_report_exec): Ditto.
      (__stap_utrace_task_finder_report_death): Ditto.
      (__stp_utrace_task_finder_target_death): Ditto.
      (__stp_utrace_task_finder_target_quiesce): Ditto.
      (__stp_utrace_task_finder_target_syscall_entry): Ditto.
      (__stp_utrace_task_finder_target_syscall_exit): Ditto.
    * runtime/uprobes2/uprobes.c (uprobe_report_signal): Ditto.
      (uprobe_report_quiesce): Ditto.
      (uprobe_report_exit): Ditto.
      (uprobe_report_clone): Ditto.
      (uprobe_report_exec): Ditto.

--- tapset-utrace.cxx
+++ tapset-utrace.cxx
@@ -842,7 +842,11 @@ utrace_derived_probe_group::emit_module_decls (systemtap_session& s)
       s.op->newline() << "#ifdef UTRACE_ORIG_VERSION";
       s.op->newline() << "static u32 stap_utrace_probe_syscall(struct utrace_attached_engine *engine, struct task_struct *tsk, struct pt_regs *regs) {";
       s.op->newline() << "#else";
+      s.op->newline() << "#if defined(UTRACE_API_VERSION) && (UTRACE_API_VERSION >= 20091216)";
+      s.op->newline() << "static u32 stap_utrace_probe_syscall(u32 action, struct utrace_attached_engine *engine, struct pt_regs *regs) {";
+      s.op->newline() << "#else";
       s.op->newline() << "static u32 stap_utrace_probe_syscall(enum utrace_resume_action action, struct utrace_attached_engine *engine, struct task_struct *tsk, struct pt_regs *regs) {";
+      s.op->newline() << "#endif";
       s.op->newline() << "#endif";
 
       s.op->indent(1);
