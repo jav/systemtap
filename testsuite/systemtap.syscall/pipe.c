a5d8d92a7c3b1ad18d6da416d4bb08922578f502
Mark Wielaard <mjw@redhat.com>
2010-03-19 16:55:10 +0100

    Add testcase for syscall.pipe.

---
+++ testsuite/systemtap.syscall/pipe.c
@@ -0,0 +1,27 @@
+/* COVERAGE: pipe pipe2 */
+
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+  int pipefd[2];
+  pipefd[0] = 42;
+  pipefd[1] = 13;
+
+  pipe (pipefd);
+  //staptest// pipe (\[42, 13\]) = 0
+
+#ifdef O_CLOEXEC
+  pipe2 (pipefd, O_CLOEXEC);
+  //staptest// pipe2 (\[NNNN, NNNN\], O_CLOEXEC) = 0
+
+  pipe2 (pipefd, O_CLOEXEC|O_NONBLOCK);
+  //staptest// pipe2 (\[NNNN, NNNN\], O_NONBLOCK|O_CLOEXEC) = 0
+#endif
+
+  return 0;
+}
