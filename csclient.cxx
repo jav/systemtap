840e507309dbe7ae6a138ccc2f420cb20015dd69
Dave Brolley <brolley@redhat.com>
2011-01-19 17:15:31 -0500

    PR 11862: remove obsolete stap-client script and friends
    - Integrate stap-client-connect.c into csclient.cxx
    - Remove stap-client-connect.c

--- csclient.cxx
+++ csclient.cxx
@@ -1,6 +1,6 @@
 /*
  Compile server client functions
- Copyright (C) 2010 Red Hat Inc.
+ Copyright (C) 2010-2011 Red Hat Inc.
 
  This file is part of systemtap, and is free software.  You can
  redistribute it and/or modify it under the terms of the GNU General
@@ -142,8 +142,9 @@ static void merge_server_info (const vector<compile_server_info> &source, vector
 static void resolve_host (systemtap_session& s, compile_server_info &server, vector<compile_server_info> &servers);
 
 #if HAVE_NSS
-static const char *server_cert_nickname = "stap-server";
-
+// -----------------------------------------------------
+// NSS related code used by the compile server client
+// -----------------------------------------------------
 static void add_server_trust (systemtap_session &s, const string &cert_db_path, const vector<compile_server_info> &server_list);
 static void revoke_server_trust (systemtap_session &s, const string &cert_db_path, const vector<compile_server_info> &server_list);
 static void get_server_info_from_db (systemtap_session &s, vector<compile_server_info> &servers, const string &cert_db_path);
@@ -164,6 +165,489 @@ static string
 signing_cert_db_path ()
 {
   return SYSCONFDIR "/systemtap/staprun";
+}
+
+static const char *server_cert_nickname = "stap-server";
+
+/* Connection state.  */
+typedef struct connectionState_t
+{
+  const char *hostName;
+  PRNetAddr   addr;
+  PRUint16    port;
+  const char *infileName;
+  const char *outfileName;
+  const char *trustNewServerMode;
+} connectionState_t;
+
+#if 0 /* No client authorization */
+static char *
+myPasswd(PK11SlotInfo *info, PRBool retry, void *arg)
+{
+  char * passwd = NULL;
+
+  if ( (!retry) && arg )
+    passwd = PORT_Strdup((char *)arg);
+
+  return passwd;
+}
+#endif
+
+/* Add the server's certificate to our database of trusted servers.  */
+static SECStatus
+trustNewServer (CERTCertificate *serverCert)
+{
+  SECStatus secStatus;
+  CERTCertTrust *trust = NULL;
+  PK11SlotInfo *slot;
+
+  /* Import the certificate.  */
+  slot = PK11_GetInternalKeySlot();;
+  secStatus = PK11_ImportCert(slot, serverCert, CK_INVALID_HANDLE, "stap-server", PR_FALSE);
+  if (secStatus != SECSuccess)
+    goto done;
+  
+  /* Make it a trusted peer.  */
+  trust = (CERTCertTrust *)PORT_ZAlloc(sizeof(CERTCertTrust));
+  if (! trust)
+    {
+      secStatus = SECFailure;
+      goto done;
+    }
+
+  secStatus = CERT_DecodeTrustString(trust, "P,P,P");
+  if (secStatus != SECSuccess)
+    goto done;
+
+  secStatus = CERT_ChangeCertTrust(CERT_GetDefaultCertDB(), serverCert, trust);
+  if (secStatus != SECSuccess)
+    goto done;
+
+done:
+  if (trust)
+    PORT_Free(trust);
+  return secStatus;
+}
+
+/* Called when the server certificate verification fails. This gives us
+   the chance to trust the server anyway and add the certificate to the
+   local database.  */
+static SECStatus
+badCertHandler(void *arg, PRFileDesc *sslSocket)
+{
+  SECStatus secStatus;
+  PRErrorCode errorNumber;
+  CERTCertificate *serverCert;
+  SECItem subAltName;
+  PRArenaPool *tmpArena = NULL;
+  CERTGeneralName *nameList, *current;
+  char *expected = NULL;
+  const connectionState_t *connectionState = (connectionState_t *)arg;
+
+  errorNumber = PR_GetError ();
+  switch (errorNumber)
+    {
+    case SSL_ERROR_BAD_CERT_DOMAIN:
+      /* Since we administer our own client-side databases of trustworthy
+	 certificates, we don't need the domain name(s) on the certificate to
+	 match. If the cert is in our database, then we can trust it.
+	 Issue a warning and accept the certificate.  */
+      expected = SSL_RevealURL (sslSocket);
+      fprintf (stderr, "WARNING: The domain name, %s, does not match the DNS name(s) on the server certificate:\n", expected);
+
+      /* List the DNS names from the server cert as part of the warning.
+	 First, find the alt-name extension on the certificate.  */
+      subAltName.data = NULL;
+      serverCert = SSL_PeerCertificate (sslSocket);
+      secStatus = CERT_FindCertExtension (serverCert,
+					  SEC_OID_X509_SUBJECT_ALT_NAME,
+					  & subAltName);
+      if (secStatus != SECSuccess || ! subAltName.data)
+	{
+	  fprintf (stderr, "Unable to find alt name extension on the server certificate\n");
+	  secStatus = SECSuccess; /* Not a fatal error */
+	  break;
+	}
+
+      // Now, decode the extension.
+      tmpArena = PORT_NewArena(DER_DEFAULT_CHUNKSIZE);
+      if (! tmpArena) 
+	{
+	  fprintf (stderr, "Out of memory\n");
+	  secStatus = SECSuccess; /* Not a fatal error here */
+	  break;
+	}
+      nameList = CERT_DecodeAltNameExtension (tmpArena, & subAltName);
+      SECITEM_FreeItem(& subAltName, PR_FALSE);
+      if (! nameList)
+	{
+	  fprintf (stderr, "Unable to decode alt name extension on server certificate\n");
+	  secStatus = SECSuccess; /* Not a fatal error */
+	  break;
+	}
+
+      /* List the DNS names from the server cert as part of the warning.
+	 The names are in a circular list.  */
+      current = nameList;
+      do
+	{
+	  /* Make sure this is a DNS name.  */
+	  if (current->type == certDNSName)
+	    {
+	      fprintf (stderr, "  %.*s\n",
+		       (int)current->name.other.len, current->name.other.data);
+	    }
+	  current = CERT_GetNextGeneralName (current);
+	}
+      while (current != nameList);
+
+      /* Accept the certificate */
+      secStatus = SECSuccess;
+      break;
+
+    case SEC_ERROR_CA_CERT_INVALID:
+      /* The server's certificate is not trusted. Should we trust it? */
+      secStatus = SECFailure; /* Do not trust by default. */
+      if (! connectionState->trustNewServerMode)
+	break;
+
+      /* Trust it for this session only?  */
+      if (strcmp (connectionState->trustNewServerMode, "session") == 0)
+	{
+	  secStatus = SECSuccess;
+	  break;
+	}
+
+      /* Trust it permanently?  */
+      if (strcmp (connectionState->trustNewServerMode, "permanent") == 0)
+	{
+	  /* The user wants to trust this server. Get the server's certificate so
+	     and add it to our database.  */
+	  serverCert = SSL_PeerCertificate (sslSocket);
+	  if (serverCert != NULL)
+	    secStatus = trustNewServer (serverCert);
+	}
+      break;
+    default:
+      secStatus = SECFailure; /* Do not trust this server */
+      break;
+    }
+
+  if (expected)
+    PORT_Free (expected);
+  if (tmpArena)
+    PORT_FreeArena (tmpArena, PR_FALSE);
+
+  return secStatus;
+}
+
+static PRFileDesc *
+setupSSLSocket (connectionState_t *connectionState)
+{
+  PRFileDesc         *tcpSocket;
+  PRFileDesc         *sslSocket;
+  PRSocketOptionData	socketOption;
+  PRStatus            prStatus;
+  SECStatus           secStatus;
+
+  tcpSocket = PR_NewTCPSocket();
+  if (tcpSocket == NULL)
+    goto loser;
+
+  /* Make the socket blocking. */
+  socketOption.option = PR_SockOpt_Nonblocking;
+  socketOption.value.non_blocking = PR_FALSE;
+
+  prStatus = PR_SetSocketOption(tcpSocket, &socketOption);
+  if (prStatus != PR_SUCCESS)
+    goto loser;
+
+  /* Import the socket into the SSL layer. */
+  sslSocket = SSL_ImportFD(NULL, tcpSocket);
+  if (!sslSocket)
+    goto loser;
+
+  /* Set configuration options. */
+  secStatus = SSL_OptionSet(sslSocket, SSL_SECURITY, PR_TRUE);
+  if (secStatus != SECSuccess)
+    goto loser;
+
+  secStatus = SSL_OptionSet(sslSocket, SSL_HANDSHAKE_AS_CLIENT, PR_TRUE);
+  if (secStatus != SECSuccess)
+    goto loser;
+
+  /* Set SSL callback routines. */
+#if 0 /* no client authentication */
+  secStatus = SSL_GetClientAuthDataHook(sslSocket,
+					(SSLGetClientAuthData)myGetClientAuthData,
+					(void *)certNickname);
+  if (secStatus != SECSuccess)
+    goto loser;
+#endif
+#if 0 /* Use the default */
+  secStatus = SSL_AuthCertificateHook(sslSocket,
+				      (SSLAuthCertificate)myAuthCertificate,
+				      (void *)CERT_GetDefaultCertDB());
+  if (secStatus != SECSuccess)
+    goto loser;
+#endif
+
+  secStatus = SSL_BadCertHook(sslSocket, (SSLBadCertHandler)badCertHandler,
+			      connectionState);
+  if (secStatus != SECSuccess)
+    goto loser;
+
+#if 0 /* No handshake callback */
+  secStatus = SSL_HandshakeCallback(sslSocket, myHandshakeCallback, NULL);
+  if (secStatus != SECSuccess)
+    goto loser;
+#endif
+
+  return sslSocket;
+
+ loser:
+  if (tcpSocket)
+    PR_Close(tcpSocket);
+  return NULL;
+}
+
+
+static SECStatus
+handle_connection (PRFileDesc *sslSocket, connectionState_t *connectionState)
+{
+  PRInt32     numBytes;
+  char       *readBuffer;
+  PRFileInfo  info;
+  PRFileDesc *local_file_fd;
+  PRStatus    prStatus;
+  SECStatus   secStatus = SECSuccess;
+
+#define READ_BUFFER_SIZE (60 * 1024)
+
+  /* read and send the data. */
+  /* Try to open the local file named.	
+   * If successful, then write it to the server
+   */
+  prStatus = PR_GetFileInfo(connectionState->infileName, &info);
+  if (prStatus != PR_SUCCESS ||
+      info.type != PR_FILE_FILE ||
+      info.size < 0)
+    {
+      fprintf (stderr, "could not find input file %s\n",
+	       connectionState->infileName);
+      return SECFailure;
+    }
+
+  local_file_fd = PR_Open(connectionState->infileName, PR_RDONLY, 0);
+  if (local_file_fd == NULL)
+    {
+      fprintf (stderr, "could not open input file %s\n", connectionState->infileName);
+      return SECFailure;
+    }
+
+  /* Send the file size first, so the server knows when it has the entire file. */
+  numBytes = htonl ((PRInt32)info.size);
+  numBytes = PR_Write(sslSocket, & numBytes, sizeof (numBytes));
+  if (numBytes < 0)
+    {
+      PR_Close(local_file_fd);
+      return SECFailure;
+    }
+
+  /* Transmit the local file across the socket.  */
+  numBytes = PR_TransmitFile(sslSocket, local_file_fd, 
+			     NULL, 0,
+			     PR_TRANSMITFILE_KEEP_OPEN,
+			     PR_INTERVAL_NO_TIMEOUT);
+  if (numBytes < 0)
+    {
+      PR_Close(local_file_fd);
+      return SECFailure;
+    }
+
+  PR_Close(local_file_fd);
+
+  /* read until EOF */
+  readBuffer = (char *)PORT_Alloc(READ_BUFFER_SIZE);
+  if (! readBuffer) {
+    fprintf (stderr, "Out of memory\n");
+    return SECFailure;
+  }
+
+  local_file_fd = PR_Open(connectionState->outfileName, PR_WRONLY | PR_CREATE_FILE | PR_TRUNCATE,
+			  PR_IRUSR | PR_IWUSR | PR_IRGRP | PR_IWGRP | PR_IROTH);
+  if (local_file_fd == NULL)
+    {
+      fprintf (stderr, "Unable to open output file %s\n", connectionState->outfileName);
+      return SECFailure;
+    }
+  while (PR_TRUE)
+    {
+      numBytes = PR_Read(sslSocket, readBuffer, READ_BUFFER_SIZE);
+      if (numBytes == 0)
+	break;	/* EOF */
+
+      if (numBytes < 0)
+	{
+	  secStatus = SECFailure;
+	  break;
+	}
+
+      /* Write to output file */
+      numBytes = PR_Write(local_file_fd, readBuffer, numBytes);
+      if (numBytes < 0)
+	{
+	  fprintf (stderr, "could not write to %s\n", connectionState->outfileName);
+	  secStatus = SECFailure;
+	  break;
+	}
+    }
+
+  PR_Free(readBuffer);
+  PR_Close(local_file_fd);
+
+  /* Caller closes the socket. */
+  return secStatus;
+}
+
+/* make the connection.
+*/
+static SECStatus
+do_connect (connectionState_t *connectionState)
+{
+  PRFileDesc *sslSocket;
+  PRStatus    prStatus;
+  SECStatus   secStatus;
+
+  secStatus = SECSuccess;
+
+  /* Set up SSL secure socket. */
+  sslSocket = setupSSLSocket (connectionState);
+  if (sslSocket == NULL)
+    return SECFailure;
+
+#if 0 /* no client authentication */
+  secStatus = SSL_SetPKCS11PinArg(sslSocket, password);
+  if (secStatus != SECSuccess)
+    goto done;
+#endif
+
+  secStatus = SSL_SetURL(sslSocket, connectionState->hostName);
+  if (secStatus != SECSuccess)
+    goto done;
+
+  prStatus = PR_Connect(sslSocket, & connectionState->addr, PR_INTERVAL_NO_TIMEOUT);
+  if (prStatus != PR_SUCCESS)
+    {
+      secStatus = SECFailure;
+      goto done;
+    }
+
+  /* Established SSL connection, ready to send data. */
+  secStatus = SSL_ResetHandshake(sslSocket, /* asServer */ PR_FALSE);
+  if (secStatus != SECSuccess)
+    goto done;
+
+  /* This is normally done automatically on the first I/O operation,
+     but doing it here catches any authentication problems early.  */
+  secStatus = SSL_ForceHandshake(sslSocket);
+  if (secStatus != SECSuccess)
+    goto done;
+
+  /* If we don't have both the input and output file names, then we're
+     contacting this server only in order to establish trust. No need to
+     handle the connection in this case.  */
+  if (connectionState->infileName && connectionState->outfileName)
+    secStatus = handle_connection(sslSocket, connectionState);
+
+ done:
+  prStatus = PR_Close(sslSocket);
+  return secStatus;
+}
+
+/* Exit error codes */
+#define SUCCESS               0
+#define GENERAL_ERROR         1
+#define CA_CERT_INVALID_ERROR 2
+
+int
+client_connect (const char *hostName, PRUint32 ip,
+	     PRUint16 port,
+	     const char* infileName, const char* outfileName,
+	     const char* trustNewServer)
+{
+  SECStatus   secStatus;
+  PRStatus    prStatus;
+  PRInt32     rv;
+  PRHostEnt   hostEntry;
+  PRErrorCode errorNumber;
+  char        buffer[PR_NETDB_BUF_SIZE];
+  int         attempt;
+  int         errCode = GENERAL_ERROR;
+  struct connectionState_t connectionState;
+
+  connectionState.hostName = hostName;
+  connectionState.port = port;
+  connectionState.infileName = infileName;
+  connectionState.outfileName = outfileName;
+  connectionState.trustNewServerMode = trustNewServer;
+
+  /* Setup network connection. If we have an ip address, then
+     simply use it, otherwise we need to resolve the host name.  */
+  if (ip)
+    {
+      connectionState.addr.inet.family = PR_AF_INET;
+      connectionState.addr.inet.port = htons (port);
+      connectionState.addr.inet.ip = htonl (ip);
+    }
+  else
+    {
+      prStatus = PR_GetHostByName(hostName, buffer, sizeof (buffer), &hostEntry);
+      if (prStatus != PR_SUCCESS) {
+	fprintf (stderr, "Unable to resolve server host name");
+	return errCode;
+      }
+
+      rv = PR_EnumerateHostEnt(0, &hostEntry, port, &connectionState.addr);
+      if (rv < 0) {
+	fprintf (stderr, "Unable to resolve server host address");
+	return errCode;
+      }
+    }
+
+  /* Some errors (see below) represent a situation in which trying again
+     should succeed. However, don't try forever.  */
+  for (attempt = 0; attempt < 5; ++attempt)
+    {
+      secStatus = do_connect (& connectionState);
+      if (secStatus == SECSuccess)
+	return SUCCESS;
+
+      errorNumber = PR_GetError ();
+      switch (errorNumber)
+	{
+	case PR_CONNECT_RESET_ERROR:
+	  /* Server was not ready. */
+	  sleep (1);
+	  break; /* Try again */
+	case SEC_ERROR_EXPIRED_CERTIFICATE:
+	  /* The server's certificate has expired. It should
+	     generate a new certificate. Give the server a chance to recover
+	     and try again.  */
+	  sleep (2);
+	  break; /* Try again */
+	case SEC_ERROR_CA_CERT_INVALID:
+	  /* The server's certificate is not trusted. The exit code must
+	     reflect this.  */
+	  errCode = CA_CERT_INVALID_ERROR;
+	  return errCode;
+	default:
+	  /* This error is fatal.  */
+	  return errCode;
+	}
+    }
+
+  return errCode;
 }
 #endif // HAVE_NSS
 
@@ -556,13 +1040,6 @@ compile_server_client::find_and_connect_to_server ()
 
   return 1; // Failure - message already generated.
 }
-
-// Temporary until the stap-client-connect program goes away.
-extern "C"
-int
-client_main (const char *hostName, PRUint32 ip, PRUint16 port,
-	     const char* infileName, const char* outfileName,
-	     const char* trustNewServer);
 #endif // HAVE_NSS
 
 // Convert the given string to an ip address in host byte order.
@@ -664,7 +1141,7 @@ compile_server_client::compile_using_server (
 	  else
 	    hostName = j->host_name;
 
-	  rc = client_main (hostName.c_str (),
+	  rc = client_connect (hostName.c_str (),
 			    stringToIpAddress (j->ip_address),
 			    j->port,
 			    client_zipfile.c_str(), server_zipfile.c_str (),
@@ -1359,7 +1836,7 @@ add_server_trust (
 
   // Iterate over the servers to become trusted. Contact each one and
   // add it to the list of trusted servers if it is not already trusted.
-  // client_main will issue any error messages.
+  // client_connect will issue any error messages.
   for (vector<compile_server_info>::const_iterator server = server_list.begin();
        server != server_list.end ();
        ++server)
@@ -1383,7 +1860,7 @@ add_server_trust (
       // number in order to contact the server.
       if (server->empty () || server->port == 0)
 	continue;
-      int rc = client_main (server->host_name.c_str (),
+      int rc = client_connect (server->host_name.c_str (),
 			    stringToIpAddress (server->ip_address),
 			    server->port,
 			    NULL, NULL, "permanent");
