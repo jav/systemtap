4c156a0e285d3853ccd514484b73fce6153081ba
Josh Stone <jistone@redhat.com>
2011-02-08 15:06:25 -0800

    Use a ControlMaster for ssh remotes
    We can start the ssh remote with a ControlMaster connection, using a
    unique socket in the tmpdir, and then each subsequent remote command can
    reuse that connection.
    
    * remote.cxx (ssh_remote::open_control_master): Connect to the remote
      host with a ControlPath that later commands can reuse.  The relevant
      options are saved in member ssh_opts.
      (ssh_remote::close_control_master): Ask the control process to exit.
      (ssh_remote::get_uname, ::start, ::finish): Start using ssh_opts, so
      the ControlPath is always specified.

--- remote.cxx
+++ remote.cxx
@@ -78,32 +78,34 @@ class direct : public remote {
       {
         string module = s->tmpdir + "/" + s->module_name + ".ko";
         pid_t pid = stap_spawn (s->verbose, make_run_command (*s, module));
-	if (pid <= 0)
-	  return 1;
-	child = pid;
-	return 0;
+        if (pid <= 0)
+          return 1;
+        child = pid;
+        return 0;
       }
 
     int finish()
       {
-	if (child <= 0)
-	  return 1;
+        if (child <= 0)
+          return 1;
 
-	int ret = stap_waitpid(s->verbose, child);
-	child = 0;
-	return ret;
+        int ret = stap_waitpid(s->verbose, child);
+        child = 0;
+        return ret;
       }
 };
 
 class ssh_remote : public remote {
     // NB: ssh commands use a tty (-t) so signals are passed along to the remote
   private:
+    string ssh_opts, ssh_control;
     string host, tmpdir;
     pid_t child;
 
     ssh_remote(systemtap_session& s, const string& host)
       : remote(s), host(host), child(0)
       {
+        open_control_master();
         get_uname();
       }
 
@@ -120,24 +122,69 @@ class ssh_remote : public remote {
           throw runtime_error("ssh target URI doesn't support a #fragment");
 
         host = ud.authority;
+        open_control_master();
         get_uname();
+      }
+
+    void open_control_master()
+      {
+        static unsigned index = 0;
+
+        if (s->tmpdir.empty()) // sanity check, shouldn't happen
+          throw runtime_error("No tmpdir available for ssh control master");
+
+        ssh_control = s->tmpdir + "/ssh_remote_control_" + lex_cast(++index);
+        ssh_opts = " -q -o ControlPath=" + lex_cast_qstring(ssh_control) + " ";
+
+        // NB: ssh -f will stay in the foreground until authentication is
+        // complete and the control socket is created, so we know it's ready to
+        // go when stap_system returns.
+        string cmd = "ssh -f -N -M " + ssh_opts + lex_cast_qstring(host);
+        int rc = stap_system(s->verbose, cmd);
+        if (rc != 0)
+          {
+            ostringstream err;
+            err << "failed to create an ssh control master for " << host
+                << " : rc=" << rc;
+            throw runtime_error(err.str());
+          }
+
+        if (s->verbose>1)
+          clog << "Created ssh control master at "
+               << lex_cast_qstring(ssh_control) << endl;
+      }
+
+    void close_control_master()
+      {
+        if (ssh_control.empty())
+          return;
+
+        string cmd = "ssh " + ssh_opts + lex_cast_qstring(host)
+          + " -O exit 2>/dev/null >/dev/null";
+        int rc = stap_system(s->verbose, cmd);
+        if (rc != 0)
+          cerr << "failed to stop the ssh control master for " << host
+               << " : rc=" << rc << endl;
+
+        ssh_control.clear();
+        ssh_opts.clear();
       }
 
     void get_uname()
       {
         ostringstream out;
         vector<string> uname;
-        string uname_cmd = "ssh -t -q " + lex_cast_qstring(host) + " uname -rm";
+        string uname_cmd = "ssh -t " + ssh_opts + lex_cast_qstring(host) + " uname -rm";
         int rc = stap_system_read(s->verbose, uname_cmd, out);
         if (rc == 0)
           tokenize(out.str(), uname, " \t\r\n");
         if (uname.size() != 2)
           throw runtime_error("failed to get uname from " + host
                               + " : rc=" + lex_cast(rc));
-        string release = uname[0];
-        string arch = uname[1];
-	// XXX need to deal with command-line vs. implied arch/release
-	this->s = s->clone(arch, release);
+        const string& release = uname[0];
+        const string& arch = uname[1];
+        // XXX need to deal with command-line vs. implied arch/release
+        this->s = s->clone(arch, release);
       }
 
   public:
@@ -156,7 +203,7 @@ class ssh_remote : public remote {
         {
           ostringstream out;
           vector<string> vout;
-          string cmd = "ssh -t -q " + qhost + " mktemp -d -t stapXXXXXX";
+          string cmd = "ssh -t " + ssh_opts + qhost + " mktemp -d -t stapXXXXXX";
           rc = stap_system_read(s->verbose, cmd, out);
           if (rc == 0)
             tokenize(out.str(), vout, "\r\n");
@@ -172,7 +219,7 @@ class ssh_remote : public remote {
 
         // Transfer the module.  XXX and uprobes.ko, sigs, etc.
         if (rc == 0) {
-          string cmd = "scp -q " + localmodule + " " + qhost + ":" + tmpmodule;
+          string cmd = "scp " + ssh_opts + localmodule + " " + qhost + ":" + tmpmodule;
           rc = stap_system(s->verbose, cmd);
           if (rc != 0)
             cerr << "failed to copy the module to " << host
@@ -181,48 +228,50 @@ class ssh_remote : public remote {
 
         // Run the module on the remote.
         if (rc == 0) {
-          string cmd = "ssh -t -q " + qhost + " "
+          string cmd = "ssh -t " + ssh_opts + qhost + " "
             + lex_cast_qstring(make_run_command(*s, tmpmodule));
           pid_t pid = stap_spawn(s->verbose, cmd);
           if (pid > 0)
-	    child = pid;
-	  else
-	    {
-	      cerr << "failed to run the module on " << host
-		   << " : ret=" << pid << endl;
-	      rc = -1;
-	    }
+            child = pid;
+          else
+            {
+              cerr << "failed to run the module on " << host
+                   << " : ret=" << pid << endl;
+              rc = -1;
+            }
         }
 
-	return rc;
+        return rc;
       }
 
     int finish()
       {
-	int rc = 0;
+        int rc = 0;
 
-	if (child > 0)
-	  {
-	    rc = stap_waitpid(s->verbose, child);
-	    child = 0;
-	  }
+        if (child > 0)
+          {
+            rc = stap_waitpid(s->verbose, child);
+            child = 0;
+          }
 
         if (!tmpdir.empty())
-	  {
-	    // Remove the tempdir.
-	    // XXX need to make sure this runs even with e.g. CTRL-C exits
-	    string qhost = lex_cast_qstring(host);
-	    string cmd = "ssh -t -q " + qhost + " rm -r " + tmpdir;
-	    tmpdir.clear();
-	    int rc2 = stap_system(s->verbose, cmd);
-	    if (rc2 != 0)
-	      cerr << "failed to delete the tempdir on " << host
-		   << " : rc=" << rc2 << endl;
-	    if (rc == 0)
-	      rc = rc2;
-	  }
+          {
+            // Remove the tempdir.
+            // XXX need to make sure this runs even with e.g. CTRL-C exits
+            string qhost = lex_cast_qstring(host);
+            string cmd = "ssh -t " + ssh_opts + qhost + " rm -r " + tmpdir;
+            tmpdir.clear();
+            int rc2 = stap_system(s->verbose, cmd);
+            if (rc2 != 0)
+              cerr << "failed to delete the tempdir on " << host
+                   << " : rc=" << rc2 << endl;
+            if (rc == 0)
+              rc = rc2;
+          }
 
-	return rc;
+        close_control_master();
+
+        return rc;
       }
 };
 
