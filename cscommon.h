5bd5df9240c55364d13c7a3d06fb5e17df939764
Dave Brolley <brolley@redhat.com>
2011-06-28 14:21:07 -0400

    Update client/server actions for protocol version 1.1.

--- cscommon.h
+++ cscommon.h
@@ -11,12 +11,15 @@
 // Version 1.1
 //   Client:
 //     - Passes localization variables to the server in the file client_tmpdir + "/locale"
+//     - Looks for 'version' tag in server's avahi record and does not automatically connect to
+//       an incompatioble server.
 //   Server:
 //   - Applies localization variables passed from the client to stap during translation.
 //   - Uses --tmpdir to specify temp directory to be used by stap, instead of -k, in order to avoid
 //     parsing error messages in search of stap's randomly-generated temp dir. As a result, the
 //     client no longer needs to remove stap's "Keeping temporary directory ..." message from the
 //     server's stderr response.
+//   - Advertises its protocol version using a 'version' tag in avahi.
 //
 #define CURRENT_CS_PROTOCOL_VERSION CS_PROTOCOL_VERSION (1, 1)
 
