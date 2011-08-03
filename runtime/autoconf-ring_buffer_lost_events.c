1c10834162ecc5c51737a86ab934ca3ed7f4e9d3
Mark Wielaard <mjw@redhat.com>
2011-08-03 22:45:11 +0200

    Fix autoconf-ring_buffer_lost_events.c compile warning.
    Newer GCC will warn about the event variable not being used,
    making this test fail. Just remove the 'event' variable.

--- runtime/autoconf-ring_buffer_lost_events.c
+++ runtime/autoconf-ring_buffer_lost_events.c
@@ -3,9 +3,8 @@
 #include <linux/types.h>
 #include <linux/ring_buffer.h>
 
-void foo (void)
+struct ring_buffer_event *foo (void)
 {
-  struct ring_buffer_event *event;
   /* last field is not always there */
-  event = ring_buffer_peek(NULL, 1, NULL, NULL);
+  return ring_buffer_peek(NULL, 1, NULL, NULL);
 }
