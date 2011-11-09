6b8b70dad8ea18ab896905c797d90102cd4be3e6
Frank Ch. Eigler <fche@redhat.com>
2011-11-09 12:11:14 -0500

    PR13373 addendum: add back a shlib->plt->shlib call trigger for myproc test

--- testsuite/systemtap.unprivileged/libloop.c
+++ testsuite/systemtap.unprivileged/libloop.c
@@ -1,10 +1,12 @@
 #include "sys/sdt.h"
+#include <stdlib.h>
 
 inline int ilibloopfunc (void) {
   int i, j = 0;
   STAP_PROBE(_test_, ilibloopfunc_enter);
   for (i = 0; i < 10; ++i)
     j += i;
+  (void) malloc(100); /* trigger some plt activity */
   return j;
 }
 
