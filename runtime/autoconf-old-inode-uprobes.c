d3567e6845583b59d1fb577825cfd9602b332d36
Negreanu Marius <groleo@gmail.com>
2012-04-10 11:07:29 -0400

    runtime/autoconf: compensate for missing <linux/wait.h> in some inode-uprobes versions

--- runtime/autoconf-old-inode-uprobes.c
+++ runtime/autoconf-old-inode-uprobes.c
@@ -1,4 +1,5 @@
 #if defined(CONFIG_ARCH_SUPPORTS_UPROBES) && defined(CONFIG_UPROBES)
+#include <linux/wait.h>
 #include <linux/uprobes.h>
 /* Check whether we have the old inode-uprobes api.
  * (It was later changed to uprobe_register and uprobe_unregister.)
